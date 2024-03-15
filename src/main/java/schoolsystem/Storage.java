906bb8385eb7aecc6eed1b6de2c6e77fade42ff6
Mikail Ozkan <ozkan.mkl.ozkanQgmail.com>
2024-03-15 14:50:23 +0100

    v3

--- src/main/java/schoolsystem/Storage.java
+++ src/main/java/schoolsystem/Storage.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 
 public class Storage {
 
-    Scanner scan = new Scanner(System.in);
+    static Scanner scan = new Scanner(System.in);
     public static void mainMenu() {
 
+        String choice = "";
+
+        do {
         System.out.println("============= STAR HIGH SCHOOL  =============\n" +
                 "============= MAIN PAGE =============\n" +
                 "\n" +
@@ -15,6 +18,23 @@ public class Storage {
                 "\t   3-Student Page\t\t \n" +
                 "\t   Q-Exit\n");
         System.out.println("Please, make a choice from the Menu");
+        choice= scan.nextLine();
+
+        switch (choice){
+            case "1"://School information
+                break;
+            case "2"://Teacher Page
+                break;
+            case "3"://Student Page
+                break;
+            case "q":
+            case "Q":
+                break;
+            default:
+                System.out.println("Please enter a valid choice");
+        }
+
+        }while(!choice.equalsIgnoreCase("q"));
 
 
     }

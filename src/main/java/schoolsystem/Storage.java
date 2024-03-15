14e28edb240f60adde6c7769ccc379959ca65877
Mikail Ozkan <ozkan.mkl.ozkanQgmail.com>
2024-03-15 14:39:45 +0100

    v2

---
+++ src/main/java/schoolsystem/Storage.java
@@ -0,0 +1,21 @@
+package schoolsystem;
+
+import java.util.Scanner;
+
+public class Storage {
+
+    Scanner scan = new Scanner(System.in);
+    public static void mainMenu() {
+
+        System.out.println("============= STAR HIGH SCHOOL  =============\n" +
+                "============= MAIN PAGE =============\n" +
+                "\n" +
+                "\t   1-School Information\n" +
+                "\t   2-Teacher Page\n" +
+                "\t   3-Student Page\t\t \n" +
+                "\t   Q-Exit\n");
+        System.out.println("Please, make a choice from the Menu");
+
+
+    }
+}
--- src/main/java/schoolsystem/_Runner.java
+++ src/main/java/schoolsystem/_Runner.java
@@ -1,4 +1,11 @@
 package schoolsystem;
 
 public class _Runner {
+
+    public static void main(String[] args) {
+
+
+        Storage.mainMenu();
+    }
+
 }
